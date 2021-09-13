794789306b2f9c2e3d0316375a779c629e7edd54
hebbaquraishi <hebba.quraishi@gmail.com>
2021-09-13 16:12:27 +0200

    added comments

--- Automation Scripts/map_parser_json.py
+++ Automation Scripts/map_parser_json.py
@@ -1,15 +1,6 @@
 """
 Author: Hebba Quraishi
 Email: quraishi@tf.uni-freiburg.de
-
-This code is divided into 2 sections:
-Section 1:
-    The objective of this section is to take a path to a .map file as input and convert it to a JSON file
-    1) the .map file format can be found here: https://www.movingai.com/benchmarks/formats.html
-    2) sample maps can be found here: https://movingai.com/benchmarks/mapf/index.html
-
-Section 2:
-    The objective of this section is to randomly generate agents and randomly assign start and end goals for the agents
 """
 
 import json
@@ -17,17 +8,27 @@ import random
 import os
 import subprocess
 
-os.chdir("/Users/hebbaquraishi/Desktop/MAPF/Automation Scripts")
-
 
 class MapGenerator:
-    def __init__(self, path, number_of_agents, number_of_goals):
+    def __init__(self, path, number_of_agents, number_of_goals) -> None:
+        """
+        Initialise the class variables
+        # Parameters
+        #   (string) path: path to the .map file (format can be found here: https://www.movingai.com/benchmarks/formats.html)
+        #   (int) number_of_agents: number of agents to initialise on the map
+        #   (int) number_of_goals: number of goals each agent must have
+        """
         self.my_map = {"height": 0, "width": 0, "vertices": [], "obstacles": [], "water": []}
         self.path = path
         self.number_of_agents = number_of_agents
         self.number_of_goals = number_of_goals
 
-    def convert_map_to_json(self):
+    def convert_map_to_json(self) -> None:
+        """
+        Take a .map file and return a JSON representation of it.
+        Output file name: my_map.json
+        Output file location: ${WORKING_DIRECTORY}$/results/my_map.json
+        """
         input_map = open(self.path, "r")
         map_row = 0
 
@@ -68,15 +69,19 @@ class MapGenerator:
 
 
 if __name__ == "__main__":
-
     max_goals = 20
     max_agents = 20
+    # Set working directory
+    os.chdir("/Users/hebbaquraishi/Desktop/MAPF/Automation Scripts")
+    # path to C++ executable file
     executable_path = "/Users/hebbaquraishi/Desktop/MAPF/CBS/MAPF"
+    # path to .map file
+    map_path = "/Users/hebbaquraishi/Desktop/MAPF/Automation Scripts/maps/random-32-32-20.map"
 
     for a in range(2, max_agents):
         for g in range(2, max_goals):
             for n in range(1, 10):
-                obj = MapGenerator(path="/Users/hebbaquraishi/Desktop/MAPF/Automation Scripts/maps/random-32-32-20.map", number_of_agents=a, number_of_goals=g)
+                obj = MapGenerator(path=map_path, number_of_agents=a, number_of_goals=g)
                 obj.convert_map_to_json()
                 obj.create_agents_json()
                 runcpp = subprocess.Popen([executable_path])
